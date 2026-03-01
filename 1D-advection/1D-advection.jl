7dbda450dc224373f42ef9c98e4d288fd0a2371b
John Omotani <john.omotani@ukaea.uk>
2026-03-01 22:37:28 +0000

    Plot profile as constant within each grid cell

--- 1D-advection/1D-advection.jl
+++ 1D-advection/1D-advection.jl
@@ -13,12 +13,27 @@ function animate_array(t, x, array)
     steps_per_frame = max(floor(Int64, 0.02 / d_t), 1)
     animation_d_t = steps_per_frame * d_t
 
-    array_observable = @lift @view array[:,$i]
+    d_x = x[2] - x[1]
+    n_x = length(x)
+
+    # Positions of cell edges
+    x_edges = zeros(2 * n_x)
+    for i_x in 1:n_x
+        x_edges[2*i_x-1] = x[i_x] - 0.5 * d_x
+        x_edges[2*i_x] = x[i_x] + 0.5 * d_x
+    end
+
+    function get_stepped_array_slice(i_t)
+        array_slice = @view array[:,i_t]
+        return [array_slice[div(i_x,2)] for i_x in 2:2*n_x+1]
+    end
+
+    array_observable = @lift get_stepped_array_slice($i)
     title = @lift string(@sprintf "t = %.3f" t[$i])
 
     fig = Figure()
     ax = Axis(fig[1,1]; title=title)
-    lines!(ax, x, array_observable)
+    lines!(ax, x_edges, array_observable)
     display(fig)
 
     for i[] in 1:steps_per_frame:n_t
