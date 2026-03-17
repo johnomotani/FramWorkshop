ef7087d7aeb03d38e22f835fe12c3f83afceb273
John Omotani <john.omotani@ukaea.uk>
2026-03-17 20:56:36 +0000

    Tidy up wave.py
    Also add some print statements to indicate progress.

--- wave/wave.py
+++ wave/wave.py
@@ -2,127 +2,21 @@ import numpy as np
 import animatplot as amp
 from matplotlib import pyplot as plt
 
-"""
-Simulate propagation of a wave in 1d, for example a string that is under tension.
-
-Boundary condition is that there is no displacement at the end points of the grid.
-
-Uses for loops for all operations.
-"""
 def simulate_wave_with_loops(wave_speed, timestep_size, n_gridpoints):
+    # Length of the string.
     L = 1.0
+
+    # Total time for the simulation.
     total_time = 100.0
 
+    print("Initialising arrays...")
+    # Array with time for each step.
     n_t = int(total_time / timestep_size)
-    time = np.fromiter((i_t * timestep_size for i_t in range(n_t)), float)
+    time = np.array([i_t * timestep_size for i_t in range(n_t)])
 
-    grid_spacing = L / (n_gridpoints - 1)
-    grid = np.fromiter((i * grid_spacing for i in range(n_gridpoints)), float)
-
-    # Array that will store values of the displacement d for all time points.
-    d = np.zeros([n_t, n_gridpoints])
-
-    # Array that will store values of the velocity v for all time points.
-    v = np.zeros([n_t, n_gridpoints])
-
-    # Set up initial profile of d and v
-    for i in range(n_gridpoints):
-        # Add some small modifications to a bell curve to make sure the initial profile
-        # is exactly zero at the boundaries.
-        d[0,i] = np.exp(-(grid[i] - L/4)**2 * 16**2) - np.exp(-(0.0 - L/4)**2 * 16**2) - np.exp(-(L - L/4)**2 * 16**2) * grid[i] / L
-
-        # v is chosen to make a right-moving disturbance at the initial time.
-        v[0,i] = -wave_speed * (-2 * (grid[i] - L/4) * 16**2 * np.exp(-(grid[i] - L/4)**2 * 16**2) - np.exp(-(L - L/4)**2 * 16**2) / L)
-
-    # Check that boundary conditions are exactly obeyed.
-    d[0,0] = 0.0
-    d[0,-1] = 0.0
-    v[0,0] = 0.0
-    v[0,-1] = 0.0
-
-    # Step through all time points
-    for i_t in range(0, n_t - 1):
-        # Note we do not update the first and last grid points as these are fixed to
-        # 0.0.
-        for i in range(1,n_gridpoints-1):
-            # v is changed by forces from the neighbouring grid points
-            displacement_left = d[i_t,i-1] - d[i_t,i]
-            displacement_right = d[i_t,i+1] - d[i_t,i]
-            v[i_t+1,i] = v[i_t,i] + wave_speed**2 / grid_spacing**2 * (displacement_left + displacement_right) * timestep_size
-
-        for i in range(1,n_gridpoints-1):
-            # d is moved by the current v
-            d[i_t+1,i] = d[i_t,i] + v[i_t+1,i] * timestep_size
-
-    print(f"timestep_size={timestep_size}, CFL-limit={grid_spacing/wave_speed}")
-
-    return time, grid, d, v
-
-"""
-Simulate propagation of a wave in 1d, for example a string that is under tension.
-
-Boundary condition is that there is no displacement at the end points of the grid.
-
-Uses numpy array operations for efficiency.
-"""
-def simulate_wave_with_array_operations(wave_speed, timestep_size, n_gridpoints):
-    L = 1.0
-    total_time = 100.0
-
-    n_t = int(total_time / timestep_size)
-    time = np.fromiter((i_t * timestep_size for i_t in range(n_t)), float)
-
-    grid_spacing = L / (n_gridpoints - 1)
-    grid = np.fromiter((i * grid_spacing for i in range(n_gridpoints)), float)
-
-    # Array that will store values of the displacement d for all time points.
-    d = np.zeros([n_t, n_gridpoints])
-
-    # Array that will store values of the velocity v for all time points.
-    v = np.zeros([n_t, n_gridpoints])
-
-    # Set up initial profile of d and v
-    #
-    # Add some small modifications to a bell curve to make sure the initial profile
-    # is exactly zero at the boundaries.
-    d[0,:] = np.exp(-(grid - L/4)**2 * 16**2) - np.exp(-(0.0 - L/4)**2 * 16**2) - np.exp(-(L - L/4)**2 * 16**2) * grid / L
-
-    # v is chosen to make a right-moving disturbance at the initial time.
-    v[0,:] = -wave_speed * (-2 * (grid - L/4) * 16**2 * np.exp(-(grid - L/4)**2 * 16**2) - np.exp(-(L - L/4)**2 * 16**2) / L)
-
-    # Check that boundary conditions are exactly obeyed.
-    d[0,0] = 0.0
-    d[0,-1] = 0.0
-    v[0,0] = 0.0
-    v[0,-1] = 0.0
-
-    # Step through all time points
-    for i_t in range(0, n_t - 1):
-        #print(time[i_t])
-        # Note we do not update the first and last grid points as these are fixed to
-        # 0.0.
-
-        # v is changed by forces from the neighbouring grid points
-        displacement_left = d[i_t,:-2] - d[i_t,1:-1]
-        displacement_right = d[i_t,2:] - d[i_t,1:-1]
-        v[i_t+1,1:-1] = v[i_t,1:-1] + wave_speed**2 / grid_spacing**2 * (displacement_left + displacement_right) * timestep_size
-
-        # d is moved by the updated v
-        d[i_t+1,1:-1] = d[i_t,1:-1] + v[i_t+1,1:-1] * timestep_size
-
-    print(f"timestep_size={timestep_size}, CFL-limit={grid_spacing/wave_speed}")
-
-    return time, grid, d, v
-
-def simulate_wave_with_array_operations_periodic(wave_speed, timestep_size, n_gridpoints):
-    L = 1.0
-    total_time = 100.0
-
-    n_t = int(total_time / timestep_size)
-    time = np.fromiter((i_t * timestep_size for i_t in range(n_t)), float)
-
+    # Create equally-spaced grid of points.
     grid_spacing = L / n_gridpoints
-    grid = np.fromiter((i * grid_spacing for i in range(n_gridpoints)), float)
+    grid = np.array([i * grid_spacing for i in range(n_gridpoints)])
 
     # Array that will store values of the displacement d for all time points.
     d = np.zeros([n_t, n_gridpoints])
@@ -131,27 +25,82 @@ def simulate_wave_with_array_operations_periodic(wave_speed, timestep_size, n_gr
     v = np.zeros([n_t, n_gridpoints])
 
     # Set up initial profile of d and v
-    #
-    # Add some small modifications to a bell curve to make sure the initial profile
-    # is exactly zero at the boundaries.
-    peak_sharpness_setting = 16
+    # This function produces a peak at L=0.25
     def initial_d(x):
-        #return np.exp(-(x - L/4)**2 * peak_sharpness_setting**2) - np.exp(-(0.0 - L/4)**2 * peak_sharpness_setting**2) - np.exp(-(L - L/4)**2 * peak_sharpness_setting**2) * x / L
-        #return np.sin(2 * np.pi * x / L)
-        return np.sin(np.pi * (x / L + 0.25))**peak_sharpness_setting
+        return np.sin(np.pi * (x / L + 0.25))**16
 
     d[0,:] = initial_d(grid)
 
     # v is chosen to make a right-moving disturbance at the initial time.
-    #v[0,:] = -wave_speed * (-2 * (grid - L/4) * peak_sharpness_setting**2 * np.exp(-(grid - L/4)**2 * peak_sharpness_setting**2) - np.exp(-(L - L/4)**2 * peak_sharpness_setting**2) / L)
-    #v[0,:] = -wave_speed * 2 * np.pi / L * np.cos(2 * np.pi * grid / L)
     initial_v_grid = (grid + wave_speed * 0.5 * timestep_size) % L
-    v[0,:] = -peak_sharpness_setting * wave_speed * np.pi / L * np.cos(np.pi * (initial_v_grid / L + 0.25)) * np.sin(np.pi * (initial_v_grid / L + 0.25))**(peak_sharpness_setting - 1)
+    v[0,:] = -16 * wave_speed * np.pi / L * np.cos(np.pi * (initial_v_grid / L + 0.25)) * np.sin(np.pi * (initial_v_grid / L + 0.25))**15
+
+    #print(f"timestep_size={timestep_size}, grid_spacing={grid_spacing}, CFL-limit={grid_spacing/wave_speed}")
 
     # Step through all time points
+    print("Start time loop")
     for i_t in range(0, n_t - 1):
-        #print(time[i_t])
+        for i in range(0, n_gridpoints):
+            # v is changed by forces from the neighbouring grid points
+            if i == 0:
+                # Periodicity means 'left' point is at the other end of the grid.
+                displacement_left = d[i_t,-1] - d[i_t,i]
+            else:
+                displacement_left = d[i_t,i-1] - d[i_t,i]
+            if i == n_gridpoints - 1:
+                # Periodicity means 'right' point is at the other end of the grid.
+                displacement_right = d[i_t,0] - d[i_t,i]
+            else:
+                displacement_right = d[i_t,i+1] - d[i_t,i]
+            v[i_t+1,i] = v[i_t,i] + wave_speed**2 / grid_spacing**2 * (displacement_left + displacement_right) * timestep_size
 
+        for i in range(0, n_gridpoints):
+            # d is moved by the updated v
+            d[i_t+1,i] = d[i_t,i] + v[i_t+1,i] * timestep_size
+
+    print("Calculate exact solution")
+    exact_d = initial_d(grid[None,:] - wave_speed * time[:,None])
+
+    return time, grid, d, v, exact_d
+
+def simulate_wave_with_array_operations(wave_speed, timestep_size, n_gridpoints):
+    # Length of the string.
+    L = 1.0
+
+    # Total time for the simulation.
+    total_time = 100.0
+
+    print("Initialising arrays...")
+    # Array with time for each step.
+    n_t = int(total_time / timestep_size)
+    time = np.array([i_t * timestep_size for i_t in range(n_t)])
+
+    # Create equally-spaced grid of points.
+    grid_spacing = L / n_gridpoints
+    grid = np.array([i * grid_spacing for i in range(n_gridpoints)])
+
+    # Array that will store values of the displacement d for all time points.
+    d = np.zeros([n_t, n_gridpoints])
+
+    # Array that will store values of the velocity v for all time points.
+    v = np.zeros([n_t, n_gridpoints])
+
+    # Set up initial profile of d and v
+    # This function produces a peak at L=0.25
+    def initial_d(x):
+        return np.sin(np.pi * (x / L + 0.25))**16
+
+    d[0,:] = initial_d(grid)
+
+    # v is chosen to make a right-moving disturbance at the initial time.
+    initial_v_grid = (grid + wave_speed * 0.5 * timestep_size) % L
+    v[0,:] = -16 * wave_speed * np.pi / L * np.cos(np.pi * (initial_v_grid / L + 0.25)) * np.sin(np.pi * (initial_v_grid / L + 0.25))**15
+
+    #print(f"timestep_size={timestep_size}, grid_spacing={grid_spacing}, CFL-limit={grid_spacing/wave_speed}")
+
+    # Step through all time points
+    print("Start time loop")
+    for i_t in range(0, n_t - 1):
         # v is changed by forces from the neighbouring grid points
         displacement_left = np.roll(d[i_t,:], 1) - d[i_t,:]
         displacement_right = np.roll(d[i_t,:], -1) - d[i_t,:]
@@ -160,13 +109,14 @@ def simulate_wave_with_array_operations_periodic(wave_speed, timestep_size, n_gr
         # d is moved by the updated v
         d[i_t+1,:] = d[i_t,:] + v[i_t+1,:] * timestep_size
 
+    print("Calculate exact solution")
     exact_d = initial_d(grid[None,:] - wave_speed * time[:,None])
-
-    print(f"timestep_size={timestep_size}, CFL-limit={grid_spacing/wave_speed}")
 
     return time, grid, d, v, exact_d
 
 def animate_results(time, grid, d, timesteps_per_frame):
+    print("Animate results.")
+
     fig = plt.figure(figsize=(8,6))
     blocks = [amp.blocks.Line(grid, d[::timesteps_per_frame,:], label="d")]
     d_max = np.max(d[::timesteps_per_frame,:])
@@ -181,6 +131,8 @@ def animate_results(time, grid, d, timesteps_per_frame):
     return
 
 def animate_results_with_error(time, grid, d, exact_d, timesteps_per_frame):
+    print("Animate results, showing error.")
+
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16,6))
 
     ax1.set_title("d")
@@ -210,11 +162,8 @@ def animate_results_with_error(time, grid, d, exact_d, timesteps_per_frame):
 
 def run_and_plot(wave_speed, timestep_size, n_gridpoints, timesteps_per_frame):
     # Run the simulation
-    #time, grid, d, v = simulate_wave_with_loops(wave_speed, timestep_size, n_gridpoints)
-    #time, grid, d, v = simulate_wave_with_array_operations(wave_speed, timestep_size, n_gridpoints)
-    time, grid, d, v, exact_d = simulate_wave_with_array_operations_periodic(wave_speed, timestep_size, n_gridpoints)
-    #time, grid, d, v = simulate_gas_wave_with_array_operations_periodic(wave_speed, timestep_size, n_gridpoints)
-    #time, grid, d, v = simulate_gas_wave_with_array_operations_periodic_staggered(wave_speed, timestep_size, n_gridpoints)
+    #time, grid, d, v, exact_d = simulate_wave_with_loops(wave_speed, timestep_size, n_gridpoints)
+    time, grid, d, v, exact_d = simulate_wave_with_array_operations(wave_speed, timestep_size, n_gridpoints)
 
     # Make a move of the results
     animate_results(time, grid, d, timesteps_per_frame)
