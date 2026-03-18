eec647ea67bb2cbd7daef9badc86e414efd31943
John Omotani <john.omotani@ukaea.uk>
2026-03-18 16:45:25 +0000

    Improve plot_moment_kinetics.py

--- mk_stuff/plot_moment_kinetics.py
+++ mk_stuff/plot_moment_kinetics.py
@@ -1,8 +1,11 @@
+import animatplot as amp
 import h5py
+from matplotlib import pyplot as plt
+import numpy as np
+import sys
 
-def load_moment_kinetics_output():
-    data_path = "../../moment_kinetics-test3/runs/sound-wave/sound-wave.moments.h5"
-    with h5py.Dataset(data_path, "r") as dataset:
+def load_moment_kinetics_output(data_path):
+    with h5py.File(data_path, "r") as dataset:
         time = dataset["dynamic_data/time"][:]
         grid = dataset["coords/z/grid"][:]
         pressure = dataset["dynamic_data/pressure"][:,0,0,:]
@@ -13,10 +16,13 @@ def animate_results(time, grid, pressure, fps=60):
     timestep_size = time[1] - time[0]
     steps_per_output = max(int(1 / (timestep_size * fps)), 1)
     blocks = [amp.blocks.Line(grid, pressure[::steps_per_output,:], label="pressure")]
-    d_max = np.max(pressure[0,:])
-    extra_space = 0.1 * d_max
+    plt.xlabel("x")
+    plt.ylabel("pressure")
+    d_max = np.max(pressure)
+    d_min = np.min(pressure)
+    extra_space = 0.1 * (d_max - d_min)
     d_max += extra_space
-    d_min = np.min(pressure[0,:]) - extra_space
+    d_min -= extra_space
     plt.ylim(d_min, d_max)
     plt.legend(loc="lower right")
     timeline = amp.Timeline(time[::steps_per_output], fps=fps)
@@ -25,6 +31,5 @@ def animate_results(time, grid, pressure, fps=60):
     plt.show()
     return
 
-def load_and_plot():
-    time, grid, pressure = load_moment_kinetics_output()
-    animate_results(time, grid, pressure)
+time, grid, pressure = load_moment_kinetics_output(sys.argv[1])
+animate_results(time, grid, pressure)
